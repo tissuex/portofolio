3d749c336736036e2c45d9d9183a79f6a8bc0c93
tissuex <aryani717273@email.com>
2026-01-19 21:48:32 +0700

    Initial portfolio website

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+document.querySelectorAll('.fade-in')
+    .forEach(el => observer.observe(el));
